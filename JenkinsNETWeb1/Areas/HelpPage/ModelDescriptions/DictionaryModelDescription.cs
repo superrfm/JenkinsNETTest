6fd08a1eaf4f307b94125651afe82a5dc9681856
Rafael Magalhães <rmagalhaes@pagbem.com.br>
2020-08-05 14:17:48 -0300

    Add project files.

---
+++ JenkinsNETWeb1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JenkinsNETWeb1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ JenkinsNETWeb2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsNETWeb2.MvcApplication" Language="C#" %>
